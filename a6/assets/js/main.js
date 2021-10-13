2ab293e7923829f2bdba1bdc52edf28bae26b6ca
miramookerjee8000@gmail.com <miramookerjee8000@gmail.com>
2021-10-13 15:46:26 -0400

    refactored code to include helper functions

--- a6/assets/js/main.js
+++ a6/assets/js/main.js
@@ -8,11 +8,12 @@ function addToCart() {
 	cartElement.textContent = "(" + newAmount + ")"
 } 
 
-function deselectIfQuantitySelected(id) {
-	// helper function that is called when a quantity is selected. 
+// Buttons
+
+function deselectIfSelected(id, unselected) {
+	// helper function that is called when a quantity or glaze is selected. 
 	// If a different button is already selected, this function will deselect it
 	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
 	var button = document.getElementById(id)
 	if (button.style.background === selected) {
 		
@@ -20,7 +21,10 @@ function deselectIfQuantitySelected(id) {
 	}
 }
 
+// Quantity
+
 function selectQuantityOne() {
+	/*
 	var selected = "rgb(240, 215, 185)"
 	var unselected = "rgb(255, 255, 255)"
 	var one_button = document.getElementById("one")
@@ -31,15 +35,21 @@ function selectQuantityOne() {
 	else {
 		one_button.style.background = selected
 		// check if each of the other buttons are already selected
-		deselectIfQuantitySelected("three") 
-		deselectIfQuantitySelected("six")
-		deselectIfQuantitySelected("twelve")
+		deselectIfSelected("three", "rgb(255, 255, 255)") 
+		deselectIfSelected("six", "rgb(255, 255, 255)") 
+		deselectIfSelected("twelve", "rgb(255, 255, 255)") 
 	}
+	*/
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(255, 255, 255)"
+	var others = ["three", "six", "twelve"]
+	selectButtonHelper(selected, unselected, "one", others)
 	
 }
 
 
 function selectQuantityThree() {
+	/*
 	var selected = "rgb(240, 215, 185)"
 	var unselected = "rgb(255, 255, 255)"
 	var three_button = document.getElementById("three")
@@ -49,14 +59,20 @@ function selectQuantityThree() {
 	else {
 		three_button.style.background = selected
 		// check if each of the other buttons are already selected
-		deselectIfQuantitySelected("one") 
-		deselectIfQuantitySelected("six")
-		deselectIfQuantitySelected("twelve")
+		deselectIfSelected("one", "rgb(255, 255, 255)") 
+		deselectIfSelected("six", "rgb(255, 255, 255)") 
+		deselectIfSelected("twelve", "rgb(255, 255, 255)") 
 	}
+	*/
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(255, 255, 255)"
+	var others = ["one", "six", "twelve"]
+	selectButtonHelper(selected, unselected, "three", others)
 }
 
 
 function selectQuantitySix() {
+	/*
 	var selected = "rgb(240, 215, 185)"
 	var unselected = "rgb(255, 255, 255)"
 	var five_button = document.getElementById("six")
@@ -66,14 +82,20 @@ function selectQuantitySix() {
 	else {
 		five_button.style.background = selected
 		// check if each of the other buttons are already selected
-		deselectIfQuantitySelected("one") 
-		deselectIfQuantitySelected("three")
-		deselectIfQuantitySelected("twelve")
+		deselectIfSelected("one", "rgb(255, 255, 255)") 
+		deselectIfSelected("three", "rgb(255, 255, 255)") 
+		deselectIfSelected("twelve", "rgb(255, 255, 255)") 
 	}
+	*/
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(255, 255, 255)"
+	var others = ["one", "three", "twelve"]
+	selectButtonHelper(selected, unselected, "six", others)
 }
 
 
 function selectQuantityTwelve() {
+	/*
 	var selected = "rgb(240, 215, 185)"
 	var unselected = "rgb(255, 255, 255)"
 	var twelve_button = document.getElementById("twelve")
@@ -83,8 +105,29 @@ function selectQuantityTwelve() {
 	else {
 		twelve_button.style.background = selected
 		// check if each of the other buttons are already selected
-		deselectIfQuantitySelected("one") 
-		deselectIfQuantitySelected("three")
-		deselectIfQuantitySelected("six")
+		deselectIfSelected("one", "rgb(255, 255, 255)") 
+		deselectIfSelected("three", "rgb(255, 255, 255)") 
+		deselectIfSelected("six", "rgb(255, 255, 255)") 
+	}
+	*/
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(255, 255, 255)"
+	var others = ["one", "three", "six"]
+	selectButtonHelper(selected, unselected, "twelve", others)
+}
+
+function selectButtonHelper(selected, unselected, id, others) {
+	// helps button serve as radio button while mantaining styling
+	var button = document.getElementById(id)
+	if (button.style.background === selected) {
+		button.style.background = unselected
+	}
+	else {
+		button.style.background = selected
+		// check if each of the other buttons are already selected
+		others.forEach ( other => deselectIfSelected(other, unselected) )
 	}
 }
+
+// Glaze
+
