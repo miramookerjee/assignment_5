70bd1aba15d2fdd102af7782c382fed884838717
miramookerjee8000@gmail.com <miramookerjee8000@gmail.com>
2021-10-13 15:51:16 -0400

    appled helper function to implement javascript for glaze

--- a6/assets/js/main.js
+++ a6/assets/js/main.js
@@ -21,101 +21,6 @@ function deselectIfSelected(id, unselected) {
 	}
 }
 
-// Quantity
-
-function selectQuantityOne() {
-	/*
-	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
-	var one_button = document.getElementById("one")
-	if (one_button.style.background === selected) {
-		one_button.style.background = unselected
-
-	}
-	else {
-		one_button.style.background = selected
-		// check if each of the other buttons are already selected
-		deselectIfSelected("three", "rgb(255, 255, 255)") 
-		deselectIfSelected("six", "rgb(255, 255, 255)") 
-		deselectIfSelected("twelve", "rgb(255, 255, 255)") 
-	}
-	*/
-	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
-	var others = ["three", "six", "twelve"]
-	selectButtonHelper(selected, unselected, "one", others)
-	
-}
-
-
-function selectQuantityThree() {
-	/*
-	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
-	var three_button = document.getElementById("three")
-	if (three_button.style.background === selected) {
-		three_button.style.background = unselected
-	}
-	else {
-		three_button.style.background = selected
-		// check if each of the other buttons are already selected
-		deselectIfSelected("one", "rgb(255, 255, 255)") 
-		deselectIfSelected("six", "rgb(255, 255, 255)") 
-		deselectIfSelected("twelve", "rgb(255, 255, 255)") 
-	}
-	*/
-	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
-	var others = ["one", "six", "twelve"]
-	selectButtonHelper(selected, unselected, "three", others)
-}
-
-
-function selectQuantitySix() {
-	/*
-	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
-	var five_button = document.getElementById("six")
-	if (five_button.style.background === selected) {
-		five_button.style.background = unselected
-	}
-	else {
-		five_button.style.background = selected
-		// check if each of the other buttons are already selected
-		deselectIfSelected("one", "rgb(255, 255, 255)") 
-		deselectIfSelected("three", "rgb(255, 255, 255)") 
-		deselectIfSelected("twelve", "rgb(255, 255, 255)") 
-	}
-	*/
-	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
-	var others = ["one", "three", "twelve"]
-	selectButtonHelper(selected, unselected, "six", others)
-}
-
-
-function selectQuantityTwelve() {
-	/*
-	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
-	var twelve_button = document.getElementById("twelve")
-	if (twelve_button.style.background === selected) {
-		twelve_button.style.background = unselected
-	}
-	else {
-		twelve_button.style.background = selected
-		// check if each of the other buttons are already selected
-		deselectIfSelected("one", "rgb(255, 255, 255)") 
-		deselectIfSelected("three", "rgb(255, 255, 255)") 
-		deselectIfSelected("six", "rgb(255, 255, 255)") 
-	}
-	*/
-	var selected = "rgb(240, 215, 185)"
-	var unselected = "rgb(255, 255, 255)"
-	var others = ["one", "three", "six"]
-	selectButtonHelper(selected, unselected, "twelve", others)
-}
-
 function selectButtonHelper(selected, unselected, id, others) {
 	// helps button serve as radio button while mantaining styling
 	var button = document.getElementById(id)
@@ -129,5 +34,64 @@ function selectButtonHelper(selected, unselected, id, others) {
 	}
 }
 
-// Glaze
+// Quantity
 
+function selectQuantityOne() {
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(255, 255, 255)"
+	var others = ["three", "six", "twelve"]
+	selectButtonHelper(selected, unselected, "one", others)
+	
+}
+
+function selectQuantityThree() {
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(255, 255, 255)"
+	var others = ["one", "six", "twelve"]
+	selectButtonHelper(selected, unselected, "three", others)
+}
+
+
+function selectQuantitySix() {
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(255, 255, 255)"
+	var others = ["one", "three", "twelve"]
+	selectButtonHelper(selected, unselected, "six", others)
+}
+
+
+function selectQuantityTwelve() {
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(255, 255, 255)"
+	var others = ["one", "three", "six"]
+	selectButtonHelper(selected, unselected, "twelve", others)
+}
+
+// Glaze
+function selectNoGlaze() {
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(238, 235, 233)"
+	var others = ["sm", "vm", "dc"]
+	selectButtonHelper(selected, unselected, "none", others)
+}
+
+function selectSMGlaze() {
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(238, 235, 233)"
+	var others = ["none", "vm", "dc"]
+	selectButtonHelper(selected, unselected, "sm", others)
+}
+
+function selectVMGlaze() {
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(238, 235, 233)"
+	var others = ["sm", "none", "dc"]
+	selectButtonHelper(selected, unselected, "vm", others)
+}
+
+function selectDCGlaze() {
+	var selected = "rgb(240, 215, 185)"
+	var unselected = "rgb(238, 235, 233)"
+	var others = ["sm", "vm", "none"]
+	selectButtonHelper(selected, unselected, "dc", others)
+}
